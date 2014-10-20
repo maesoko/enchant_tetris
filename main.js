44a51f486c14bfba688df22fb72dc1eac01b47d4
Akinori Maesoko <s13012@std.it-college.ac.jp>
2014-10-21 00:23:13 +0900

    ブロック描画処理テスト

--- main.js
+++ main.js
@@ -3,19 +3,77 @@ enchant();
 var stage, game;
 
 gsettings = {                 
-    width:320
-   ,height:320
-   ,fps:15
+	width:320
+		,height:320
+		,fps:30
 };
 
 window.onload = function(){
 
-  game = new Core(gsettings.width,gsettings.height);
-  stage = game.rootScene;     
+	game = new Core(gsettings.width,gsettings.height);
+	game.fps = gsettings.fps;
+	stage = game.rootScene;     
 
-  stage.backgroundColor = "blue"; 
-  game.onload=function(){
-  
-  };
-  game.start();
+	game.onload=function(){
+		var sprite = new Sprite(100,100);
+		var surface = new Surface(100,100);
+		sprite.image = surface;
+
+		sprite.x = 100;
+		sprite.y = 100;
+		context = surface.context;
+
+		var matrix = blocks[Math.floor(Math.random() * blocks.length)];
+
+		for(y = 0; y < matrix.length; y++){
+			for(x = 0; x < matrix[y].length; x++){
+				if(matrix[y][x]){
+					//context.fillRect((x + 0) * 20, (y + 0) * 20, 20, 20);
+					context.fillRect(x * 20, y * 20, x + 20, y + 20);
+				}
+			}
+		}
+
+
+		stage.addChild(sprite);
+	};
+	game.start();
 };
+
+var blocks = [
+	[
+		[1,1],
+		[0,1],
+		[0,1]
+	],
+	[
+		[1,1],
+		[1,0],
+		[1,0]
+	],	
+	[
+		[1,1],
+		[1,1]
+	],
+	[
+		[1,0],
+		[1,1],
+		[1,0]
+	],
+	[
+		[1,0],
+		[1,1],
+		[0,1]
+	],
+	[
+		[0,1],
+		[1,1],
+		[1,0]
+	],
+	[
+		[1],
+		[1],
+		[1],
+		[1]
+	]
+	];
