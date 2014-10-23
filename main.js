2fe5cbdf84a8d07a7f66964ccbe0d31ccd280edc
Akinori Maesoko <s13012@std.it-college.ac.jp>
2014-10-24 02:45:09 +0900

    gameOver(ゲーム終了判定)実装

--- main.js
+++ main.js
@@ -123,12 +123,16 @@ var eSprite = Class.create(Sprite,{
         this.clearRows(blockMap);
         this.matrix = BLOCKS[random(BLOCKS.length)];
         this.color = BLOCK_COLORS[random(BLOCK_COLORS.length)];
+        if(this.gameOver()){
+          this.image.paintMatrix(this.matrix, this.color);
+          game.end();
+        }
       }
     }
 
-    this.image.paintMatrix(this.matrix, this.color);
+    this.image.paintMatrix(this.matrix, this.color); //毎フレームごとにブロックを描画
   },
-  check:function(map, block, offsetx, offsety){
+  check:function(map, block, offsetx, offsety){ //ブロックの衝突判定
     if (offsetx < 0 || offsety < 0 ||
         this.mapHeight < offsety + block.length ||
         this.mapWidth  < offsetx + block[0].length) {
@@ -153,7 +157,7 @@ var eSprite = Class.create(Sprite,{
     }
     return rotated;
   },
-  mergeMatrix:function(map, block, offsetx, offsety){
+  mergeMatrix:function(map, block, offsetx, offsety){ //blockMap配列にblock配列を複製
     for (var y = 0; y < this.mapHeight; y ++) {
       for (var x = 0; x < this.mapWidth; x ++) {
         if (block[y - offsety] && block[y - offsety][x - offsetx]) {
@@ -162,7 +166,7 @@ var eSprite = Class.create(Sprite,{
       }
     }
   },
-  clearRows:function(map){
+  clearRows:function(map){ //埋まった行を消去
     for (var y = 0; y < this.mapHeight; y ++) {
       var full = true;
       for (var x = 0; x < this.mapWidth; x ++) {
@@ -180,8 +184,14 @@ var eSprite = Class.create(Sprite,{
       }
     }
   },
-  isBlock:function(block){
+  isBlock:function(block){ //渡された引数がブロック配列かを判定
     if(block != blockMap){
+      return true;
+    }
+    return false;
+  },
+  gameOver:function(){ //ゲームオーバー判定
+    if(this.y == 0 && !this.check(blockMap, this.matrix, this.x / this.blockSize, this.y / this.blockSize)){
       return true;
     }
     return false;
@@ -206,7 +216,7 @@ var eSurface = Class.create(Surface,{
   }
 });
 
-function initGame(){
+function initGame(){ //初期化処理
   game = new Core(gsettings.width,gsettings.height);
   game.fps = gsettings.fps;
   stage = game.rootScene; 
@@ -220,7 +230,7 @@ function initGame(){
   }
 }
 
-function random(size){
+function random(size){ //配列の長さに応じて乱数返す
   return Math.floor(Math.random() * size);
 }
 
